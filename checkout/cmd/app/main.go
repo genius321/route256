199ce1eb9be991339234e9dc9939ce5d1574d1ef
Konstantin Orlov <genius4666@gmail.com>
2023-05-21 19:02:27 +0300

    implement first response

--- checkout/cmd/app/main.go
+++ checkout/cmd/app/main.go
@@ -1,7 +1,26 @@
 package main
 
-// const port = ":8080"
+import (
+	"log"
+	"net/http"
+)
+
+const port = ":8080"
+
+type addToCartHandle struct {
+}
 
 func main() {
 
+	hand := addToCartHandle{}
+	http.Handle("/addToCart", hand)
+
+	err := http.ListenAndServe(port, nil)
+	log.Fatalln("ERR: ", err)
+}
+
+func (h addToCartHandle) ServeHTTP(response http.ResponseWriter, request *http.Request) {
+	response.Header().Add("Content-Type", "application/json")
+	response.WriteHeader(http.StatusOK)
+	_, _ = response.Write([]byte("{\"status\": \"done\"}\n"))
 }

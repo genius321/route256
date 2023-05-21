cb4fc87f0f310c5331c562b5ea75c455abba306e
Konstantin Orlov <genius4666@gmail.com>
2023-05-21 21:09:11 +0300

    implement parse request

--- checkout/cmd/app/main.go
+++ checkout/cmd/app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 )
@@ -19,7 +20,39 @@ func main() {
 	log.Fatalln("ERR: ", err)
 }
 
+type Request struct {
+	User  int64  `json:"user"`
+	SKU   uint32 `json:"sku"`
+	Count uint16 `json:"count"`
+}
+
 func (h addToCartHandle) ServeHTTP(response http.ResponseWriter, request *http.Request) {
+	requestIn := Request{}
+
+	token := request.Header.Get("Authorization")
+
+	log.Printf("Token: %s", token)
+
+	err := json.NewDecoder(request.Body).Decode(&requestIn)
+	if err != nil {
+		log.Printf("ERR: parse body: %s", err)
+	}
+	// data, err := io.ReadAll(request.Body)
+	// if err != nil {
+	// 	log.Printf("ERR: parse body: %s", err)
+	// }
+	// err = json.Unmarshal(data, &requestIn)
+	// if err != nil {
+	// 	log.Printf("ERR: parse body: %s", err)
+	// }
+	log.Printf("%+v", requestIn)
+
+	if requestIn.User == 0 {
+		response.WriteHeader(http.StatusBadRequest)
+		_, _ = response.Write([]byte("User not set!\n"))
+		return
+	}
+
 	response.Header().Add("Content-Type", "application/json")
 	response.WriteHeader(http.StatusOK)
 	_, _ = response.Write([]byte("{\"status\": \"done\"}\n"))

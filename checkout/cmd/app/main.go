99e7833b12fbfcfebd089d936fbe5c60f0cd79d6
Константин Орлов <genius4666@gmail.com>
2023-06-02 19:16:05 +0000

    Revert "Merge branch 'GOHW-1' into 'master'"
    This reverts merge request !1

--- checkout/cmd/app/main.go
+++ checkout/cmd/app/main.go
@@ -1,50 +1,7 @@
 package main
 
-import (
-	"log"
-	"net/http"
-	"route256/checkout/internal/clients/loms"
-	"route256/checkout/internal/clients/productservice"
-	"route256/checkout/internal/config"
-	"route256/checkout/internal/domain"
-	"route256/checkout/internal/handlers/addtocart"
-	"route256/checkout/internal/handlers/deletefromcart"
-	"route256/checkout/internal/handlers/listcart"
-	"route256/checkout/internal/handlers/purchase"
-	"route256/libs/srvwrapper"
-)
-
-const port = ":8080"
+// const port = ":8080"
 
 func main() {
 
-	err := config.Init()
-	if err != nil {
-		log.Fatalln("ERR main config.Init:", err)
-	}
-
-	modelStockChecker := domain.NewModelStockChecker(loms.NewClientStocks(config.AppConfig.Services.Loms))
-	modelProductGetter := domain.NewModelProductGetter(productservice.NewClientGetProduct(config.AppConfig.Services.ProductService))
-	modelOrderCreater := domain.NewModelOrderCreater(loms.NewClientCreateOrder(config.AppConfig.Services.Loms))
-
-	addtocart := &addtocart.Handler{
-		Model: modelStockChecker,
-	}
-	http.Handle("/addToCart", srvwrapper.New(addtocart.Handle))
-
-	deletefromcart := &deletefromcart.Handler{}
-	http.Handle("/deleteFromCart", srvwrapper.New(deletefromcart.Handle))
-
-	listcart := &listcart.Handler{
-		Model: modelProductGetter,
-	}
-	http.Handle("/listCart", srvwrapper.New(listcart.Handle))
-
-	purchase := &purchase.Handler{
-		Model: modelOrderCreater,
-	}
-	http.Handle("/purchase", srvwrapper.New(purchase.Handle))
-
-	err = http.ListenAndServe(port, nil)
-	log.Fatalln("ERR main ListenAndServe:", err)
 }
--- loms/cmd/app/main.go
+++ loms/cmd/app/main.go
@@ -1,36 +1,7 @@
 package main
 
-import (
-	"log"
-	"net/http"
-	"route256/libs/srvwrapper"
-	"route256/loms/internal/handlers/cancelorder"
-	"route256/loms/internal/handlers/createorder"
-	"route256/loms/internal/handlers/listorder"
-	"route256/loms/internal/handlers/orderpayed"
-	"route256/loms/internal/handlers/stocks"
-)
-
-const port = ":8081"
+// const port = ":8081"
 
 func main() {
-	createorder := &createorder.Handler{}
-	http.Handle("/createOrder", srvwrapper.New(createorder.Handle))
 
-	listorder := &listorder.Handler{}
-	http.Handle("/listOrder", srvwrapper.New(listorder.Handle))
-
-	orderpayed := &orderpayed.Handler{}
-	http.Handle("/orderPayed", srvwrapper.New(orderpayed.Handle))
-
-	cancelorder := &cancelorder.Handler{}
-	http.Handle("/cancelOrder", srvwrapper.New(cancelorder.Handle))
-
-	stocks := &stocks.Handler{}
-	http.Handle("/stocks", srvwrapper.New(stocks.Handle))
-
-	err := http.ListenAndServe(port, nil)
-	if err != nil {
-		log.Fatalln("ERR main ListenAndServe:", err)
-	}
 }
